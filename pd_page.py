db1d4883ebd8469bb959cadc904b61cb7a6a234d
Котик <lands3d@ukr.net>
2025-11-29 19:05:18 +0200

    add pd page

--- pd_page.py
+++ pd_page.py
@@ -1,9 +1,11 @@
-from locators.find_bugs_locators import ProductDetailsPageLocators
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
 
+driver = webdriver.Chrome()
+driver.get("https://academybugs.com/store/dnk-yellow-shoes/")
+add_to_cart_pdp = driver.find_element(By.XPATH, "//input[@type='submit' and @value='ADD TO CART']")
+add_to_cart_pdp.click()
+time.sleep(5)
+assert "https://academybugs.com/my-cart/" in driver.current_url
 
-class PDPPage:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def click_add_to_cart(self):
-        self.driver.find_element(*ProductDetailsPageLocators.ADD_TO_CART_PDP).click()
